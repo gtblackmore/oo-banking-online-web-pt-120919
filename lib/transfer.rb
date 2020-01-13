3137779b1f2d0c3906a88dde42b9194cb22287f8
George Blackmore <gtblackmore@gmail.com>
2020-01-13 18:04:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,15 +13,19 @@ class Transfer
   
   def execute_transaction
     if self.valid? && @status == "pending" && sender.balance > @amount
-      @sender.deposit(-amount)
-      @receiver.deposit(amount)
+      @sender.deposit(-@amount)
+      @receiver.deposit(@amount)
       @status = "complete"
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
-      
-      
-      
     end
   end
+  
+  def reverse_transfer
+    if @status == "complete"
+      @sender.deposit(@amount)
+      @receiver.deposit(-@amount)
+      @status = "reversed"
+    end
 end
